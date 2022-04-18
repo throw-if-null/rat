276560482e7ffd9dadad9bee223a3137f7e7b8e5
mimme <mirzamerdovic@gmail.com>
2022-04-18 22:00:02 +0200

    Implement database schema publish

--- src/Rat.Api.Test/TestApplication.cs
+++ src/Rat.Api.Test/TestApplication.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using Dapper;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Hosting;
@@ -16,6 +18,7 @@ using Rat.Api.Auth;
 using Rat.Api.Test.Auth;
 using Rat.Api.Test.Mocks;
 using Rat.DataAccess;
+using Snapshooter;
 
 namespace Rat.Api.Test
 {
@@ -59,22 +62,44 @@ namespace Rat.Api.Test
 				var connectionFactory = scope.ServiceProvider.GetRequiredService<ISqlConnectionFactory>();
 				var logger = scopedServices.GetRequiredService<ILogger<TestApplication>>();
 
-				using var connection = connectionFactory.CreateConnection();
+				try
+				{
+					using var connection = connectionFactory.CreateConnection();
 
-				AddProjectType(connection, "js");
-				AddProjectType(connection, "csharp");
+					connection.Open();
 
-				AddUser(connection);
+					if (connection.DatabaseExists("RatDb"))
+						connection.DropDatabase("RatDb");
+
+					connection.CreateDatabase("RatDb");
+					connection.CreateTable("RatDb", "ProjectType.sql");
+					connection.CreateTable("RatDb", "Project.sql");
+					connection.CreateTable("RatDb", "ConfigurationType.sql");
+					connection.CreateTable("RatDb", "ConfigurationRoot.sql");
+					connection.CreateTable("RatDb", "ConfigurationEntry.sql");
+					connection.CreateTable("RatDb", "Member.sql");
+					connection.CreateTable("RatDb", "MemberProject.sql");
+
+					AddProjectType(connection, "RatDb", "js");
+					AddProjectType(connection, "RatDb", "csharp");
+					AddUser(connection, "RatDb");
+				}
+				catch (Exception ex)
+				{
+
+					throw;
+				}
 			});
 
 			return base.CreateHost(builder);
 		}
 
-		private static void AddProjectType(SqlConnection connection, string name)
+		private static void AddProjectType(SqlConnection connection, string database, string name)
 		{
 			var getCommand = new CommandDefinition("SELECT Id FROM ProjectType WHERE Name = @Name", new { Name = name });
 			var insertCommand = new CommandDefinition("INSERT INTO ProjectType (Name) VALUES(@Name)", new { Name = name });
 
+			connection.ChangeDatabase(database);
 			var projectTypeId = connection.QuerySingleOrDefault<int?>(getCommand);
 
 			if (projectTypeId.HasValue)
@@ -83,17 +108,63 @@ namespace Rat.Api.Test
 			connection.Execute(insertCommand);
 		}
 
-		private static void AddUser(SqlConnection connection)
+		private static void AddUser(SqlConnection connection, string database)
 		{
 			var getCommand = new CommandDefinition("SELECT Id FROM Member WHERE AuthProviderId = @AuthProviderId", new { AuthProviderId = TestUserProvider.UserId });
 			var inserCommand = new CommandDefinition("INSERT INTO Member (AuthProviderId) VALUES(@AuthProviderId)", new { AuthProviderId = TestUserProvider.UserId });
 
+			connection.ChangeDatabase(database);
 			var userId = connection.QuerySingleOrDefault<int?>(getCommand);
 
 			if (userId.HasValue)
 				return;
 
 			connection.Execute(inserCommand);
+		}
+	}
+
+	internal static class SqlConnectionExtensions
+	{
+		public static bool DatabaseExists(this SqlConnection connection, string name)
+		{
+			var command = new CommandDefinition(
+				"SELECT name FROM sys.databases WHERE name = @DatabaseName",
+				new { DatabaseName = name });
+
+			connection.ChangeDatabase("master");
+			var result = connection.QueryFirstOrDefault<string>(command);
+
+			return !string.IsNullOrWhiteSpace(result);
+		}
+
+		public static void DropDatabase(this SqlConnection connection, string name)
+		{
+			var query =
+				$@"ALTER DATABASE {name}
+				SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+				DROP DATABASE {name}";
+
+			connection.ChangeDatabase("master");
+			connection.Execute(new CommandDefinition(query));
+		}
+
+		public static void CreateDatabase(this SqlConnection connection, string name)
+		{
+			var query = $@"CREATE DATABASE {name}";
+
+			connection.ChangeDatabase("master");
+			connection.Execute(new CommandDefinition(query));
+		}
+
+		public static void CreateTable(this SqlConnection connection, string database, string name)
+		{
+			var path = Environment.GetEnvironmentVariable("DATABASE_PROJECT_PATH");
+			path = @"D:\Source\GitHub\rat\src\Rat.Database";
+
+			var query = File.ReadAllText(Path.Combine(path, "Tables", name));
+
+			connection.ChangeDatabase(database);
+			connection.Execute(new CommandDefinition(query));
 		}
 	}
 }
--- src/Rat.Database/Tables/Member.sql
+++ src/Rat.Database/Tables/Member.sql
@@ -6,8 +6,3 @@
     CONSTRAINT [PK_User_Id] PRIMARY KEY ([Id] ASC),
 	CONSTRAINT [UQ_AuthProviderId] UNIQUE ([AuthProviderId])
 )
-
-GO
-
-CREATE INDEX [IX_User_AuthProviderId] ON [dbo].[Member] ([AuthProviderId])
-GO
