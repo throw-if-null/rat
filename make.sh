26110f328ad8e91c4c1d698a88c858be4e63b3fb
mimme <mirzamerdovic@gmail.com>
2022-05-31 21:48:02 +0200

    Fix redoc trigger

--- make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Getting started"
-
-npx redoc-cli build https://rattus.azurewebsites.net/swagger/v1/swagger.yaml && \
-mv redoc-static.html index.html && \
-echo "Changed name from redoc-static.html to index.html" && \
-sed -i '7 i \ \ <link rel="icon" type="image/x-icon" href="images/favicon.ico">' index.html && \
-echo -e "\nDone!"
